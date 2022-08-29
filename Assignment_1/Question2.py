708b0ffee381a2a0cfa398fd3b9fad496b0d50d0
mxb07130 <mxb07130@ucmo.edu>
2022-08-29 01:42:02 -0500

    Update Question2.py
    question2

--- Assignment_1/Question2.py
+++ Assignment_1/Question2.py
@@ -1,2 +1,15 @@
-print('manoj')
-print('kumar')
+dog={"name","color","breed","legs","age"}
+student={"first_name":"manoj","last_name":"kumar","gender":"female","age":"30","martial status":"thrice","skills":["java","c","python"],"country":"usa","city":"kansas","address":"1004"}
+#length
+print(len(student))
+#values of skills
+print(student["skills"])
+#modify
+student["skills"].append("c sharp")
+print(student["skills"])
+#keys
+print(student.keys())
+#values
+print(student.values())
+
+
